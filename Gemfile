f0c67e74be6d4641e281dda33d944b2a405de224
uda-cha <mine_s@zd6.so-net.ne.jp>
2020-04-29 20:34:24 +0900

    enable to run on aws lambda

--- Gemfile
+++ Gemfile
@@ -6,4 +6,3 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'aws-sdk-s3'
 gem 'nokogiri'
-gem 'pry-byebug'
--- dlsite_rss.rb
+++ dlsite_rss.rb
@@ -3,11 +3,6 @@ require 'json'
 require 'nokogiri'
 require 'open-uri'
 require 'rss'
-require 'pry'
-
-rss_url = ENV['RSS_URL']
-target_url = 'https://www.dlsite.com/maniax/new/=/work_type_category/voice'
-latest_data_basename = "voice_latest_works"
 
 def s3_client
   @s3_client ||=
@@ -27,50 +22,62 @@ def put_to_s3(key:, body:, content_type: "application/json; charset=utf-8")
   )
 end
 
-charset = nil
-html = open(target_url) do |f|
-  charset = f.charset
-  f.read
-end
+def lambda_handler(event: nil, context: nil)
+  rss_url = ENV['RSS_URL']
+  target_url = 'https://www.dlsite.com/maniax/new/=/work_type_category/voice'
+  latest_data_basename = "voice_latest_works"
 
-doc = Nokogiri::HTML.parse(html, nil, charset).search('.n_worklist_item')
+  charset = nil
+  html = open(target_url) do |f|
+    charset = f.charset
+    f.read
+  end
 
-latest_works = {}
-updated_at = Time.now.strftime("%Y%m%d_%H%M%S")
-doc.each do |work|
-  node = work.search('.work_name')
-  title = node.css('a').inner_text
-  url = node.css('a').attribute('href').value
+  doc = Nokogiri::HTML.parse(html, nil, charset).search('.n_worklist_item')
 
-  latest_works[url] = { title: title, updated_at: updated_at }
-end
+  latest_works = {}
+  updated_at = Time.now.strftime("%Y%m%d_%H%M%S")
+  doc.each do |work|
+    node = work.search('.work_name')
+    title = node.css('a').inner_text
+    url = node.css('a').attribute('href').value
 
-previous_works = JSON.parse(
-  s3_client.get_object(
-    bucket: ENV['BUCKET'],
-    key: "#{latest_data_basename}.json"
-  ).body.read
-)
+    latest_works[url] = { title: title, updated_at: updated_at }
+  end
 
-latest_works.merge!(previous_works).take(20)
+  previous_works = JSON.parse(
+    begin
+      s3_client.get_object(
+        bucket: ENV['BUCKET'],
+        key: "#{latest_data_basename}.json"
+      ).body.read
+    rescue Aws::S3::Errors::NoSuchKey
+      "{}"
+    end
+  )
 
-rss = RSS::Maker.make('2.0') do |maker|
-  maker.channel.language = 'ja'
-  maker.channel.author = "uda-cha"
-  maker.channel.updated = updated_at
-  maker.channel.link = rss_url
-  maker.channel.title = "DLsite RSS Feed(Voice)"
-  maker.channel.description = "DLsite RSS Feed(Voice)"
+  latest_works.merge!(previous_works).take(20)
 
-  latest_works.each do |key, val|
-    maker.items.new_item do |item|
-      item.link = key
-      item.title = val[:title]
-      item.updated = val[:updated_at]
+  rss = RSS::Maker.make('2.0') do |maker|
+    maker.channel.language = 'ja'
+    maker.channel.author = "uda-cha"
+    maker.channel.updated = updated_at
+    maker.channel.link = rss_url
+    maker.channel.title = "DLsite RSS Feed(Voice)"
+    maker.channel.description = "DLsite RSS Feed(Voice)"
+
+    latest_works.each do |key, val|
+      maker.items.new_item do |item|
+        item.link = key
+        item.title = val[:title]
+        item.updated = val[:updated_at]
+      end
     end
   end
+
+  put_to_s3(key: "voice_rss.xml", body: rss.to_s, content_type: "application/xml")
+  put_to_s3(key: "#{latest_data_basename}.json", body: latest_works.to_json)
+  put_to_s3(key: "#{latest_data_basename}_#{updated_at}.json", body: latest_works.to_json)
 end
 
-put_to_s3(key: "voice_rss.xml", body: rss.to_s, content_type: "application/xml")
-put_to_s3(key: "#{latest_data_basename}.json", body: latest_works.to_json)
-put_to_s3(key: "#{latest_data_basename}_#{updated_at}.json", body: latest_works.to_json)
+lambda_handler if __FILE__ == $0
