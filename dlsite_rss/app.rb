377e049dd7fce7fbc03b886fb1c4f363dd1b9803
uda-cha <mine_s@zd6.so-net.ne.jp>
2020-07-29 23:48:13 +0900

    refactoring

--- dlsite_rss/app.rb
+++ dlsite_rss/app.rb
@@ -63,8 +63,27 @@ def current_time
   @current_time ||= Time.now.strftime("%Y%m%d_%H%M%S")
 end
 
+def make_rss(data:)
+  RSS::Maker.make('2.0') do |maker|
+    maker.channel.language = 'ja'
+    maker.channel.author = "uda-cha"
+    maker.channel.updated = current_time
+    maker.channel.link = ENV['RSS_URL']
+    maker.channel.title = "DLsite RSS Feed(Voice)"
+    maker.channel.description = "DLsite RSS Feed(Voice)"
+
+    data.each do |key, val|
+      maker.items.new_item do |item|
+        item.link = key
+        item.title = val[:title]
+        item.description = "[#{val[:maker]}#{" / " + val[:author] if val[:author]}] #{val[:work_text]}"
+        item.updated = val[:updated_at]
+      end
+    end
+  end
+end
+
 def lambda_handler(event: nil, context: nil)
-  rss_url = ENV['RSS_URL']
   latest_data_basename = "voice_latest_works"
 
   latest_works = parse_latest_works(url: target_url, updated_at: current_time)
@@ -79,28 +98,10 @@ def lambda_handler(event: nil, context: nil)
     end
   previous_works = JSON.parse(previous_works_json, symbolize_names: true)
   latest_works.merge!(previous_works).take(20)
-
-  rss = RSS::Maker.make('2.0') do |maker|
-    maker.channel.language = 'ja'
-    maker.channel.author = "uda-cha"
-    maker.channel.updated = current_time
-    maker.channel.link = rss_url
-    maker.channel.title = "DLsite RSS Feed(Voice)"
-    maker.channel.description = "DLsite RSS Feed(Voice)"
-
-    latest_works.each do |key, val|
-      maker.items.new_item do |item|
-        item.link = key
-        item.title = val[:title]
-        item.description = "[#{val[:maker]}#{" / " + val[:author] if val[:author]}] #{val[:work_text]}"
-        item.updated = val[:updated_at]
-      end
-    end
-  end
+  rss = make_rss(data: latest_works)
 
   put_to_s3(key: "voice_rss.xml", body: rss.to_s, content_type: "application/xml", public: true)
   put_to_s3(key: "#{latest_data_basename}.json", body: latest_works.to_json)
-  put_to_s3(key: "#{latest_data_basename}_#{current_time}.json", body: latest_works.to_json)
 end
 
 lambda_handler if __FILE__ == $0
