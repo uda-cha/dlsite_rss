9c3db8385939fe05a0fa47efd7f16c9137566189
uda-cha <mine_s@zd6.so-net.ne.jp>
2021-11-13 19:19:04 +0900

    add Accept-Encoding

--- lib/dlsite_rss/http_client.rb
+++ lib/dlsite_rss/http_client.rb
@@ -5,14 +5,14 @@ module DlsiteRss
   class HttpClient
     class << self
       def get(url)
-        HTTParty.get(url)
+        HTTParty.get(url, headers: headers)
       rescue => e
         puts "url: " + url.to_s
         raise e
       end
 
       def head(url)
-        HTTParty.head(url)
+        HTTParty.head(url, headers: headers)
       rescue => e
         puts "url: " + url.to_s
         raise e
@@ -21,6 +21,11 @@ module DlsiteRss
       def parse_with_nokogiri(url)
         res = get(url)
         Nokogiri::HTML.parse(res.body, nil, res.type_params["charset"])
+      end
+
+      private
+      def headers
+        { 'Accept-Encoding' => 'gzip,deflate,identity' }
       end
     end
   end
