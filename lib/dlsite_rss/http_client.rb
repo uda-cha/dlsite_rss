58c43f1e77eb60535bdd5ce45fab838b57d42691
uda-cha <mine_s@zd6.so-net.ne.jp>
2021-11-13 20:53:08 +0900

    retry until 3 failures

--- lib/dlsite_rss/http_client.rb
+++ lib/dlsite_rss/http_client.rb
@@ -5,16 +5,22 @@ module DlsiteRss
   class HttpClient
     class HttpError < StandardError; end
 
+    NUM_OF_RETRIES = 3
+
     class << self
       def get(url)
-        HTTParty.get(url, headers: headers).tap do |res|
-          raise HttpError.new("HTTP #{res.code}, #{url}") if res.code >= 400
+        retry_on_error do
+          raise_on_http_error do
+            HTTParty.get(url, headers: headers)
+          end
         end
       end
 
       def head(url)
-        HTTParty.head(url, headers: headers).tap do |res|
-          raise HttpError.new("HTTP #{res.code}, #{url}") if res.code >= 400
+        retry_on_error do
+          raise_on_http_error do
+            HTTParty.head(url, headers: headers)
+          end
         end
       end
 
@@ -26,6 +32,28 @@ module DlsiteRss
       private
       def headers
         { 'Accept-Encoding' => 'gzip,deflate,identity' }
+      end
+
+      def raise_on_http_error(&block)
+        res = yield
+        raise HttpError.new("HTTP #{res.code}, #{res.request.last_uri}") if res.code >= 400
+        res
+      end
+
+      def retry_on_error(&block)
+        try = 0
+        begin
+          try += 1
+          yield
+        rescue => e
+          puts e.message
+          puts "retrying after 3 seconds..."
+
+          sleep(3)
+          retry if try < NUM_OF_RETRIES
+          puts "the maximum number of retries has been exceeded!"
+          raise e
+        end
       end
     end
   end
